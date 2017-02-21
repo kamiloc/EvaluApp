f57c8edbb5a3da2a209f6a2c93a7c242ffcee5ae
kamiloc <kamilo.sniper@gmail.com>
2017-02-21 09:20:39 -0500

    Finish validationw on profileManager, change favicon

--- src/app/controllers/tutor/profileTutor.js
+++ src/app/controllers/tutor/profileTutor.js
@@ -7,7 +7,7 @@ import {Tutor} from './classTutor';
   styleUrls: ['./app/styles/tutor/profileTutor.scss']
 })
 export class ProfileTutorComponent {
-  oldTutor = new Tutor('Gabriel', 'Martines', '489-1340', 'Web UI Developer', 'UX Desing',
+  oldTutor = new Tutor('Gabriel', 'Martines', '4891340', 'Web UI Developer', 'UX Desing',
   'gabriel.martinez', 'Male', '12345678');
   tutor = new Tutor();
   newPassword = '';
@@ -18,9 +18,24 @@ export class ProfileTutorComponent {
     this.isTutor = true;
     this.tutor = this.oldTutor;
   }
-  changePassword() {
+  onChangePassword() {
     if (this.newPassword === '') {
       this.confPassword = '';
     }
+  }
+  changedPassword() {
+    let disable = true;
+    if (this.newPassword === '') {
+      disable = false;
+    } else if (this.newPassword === this.confPassword) {
+      this.tutor.password = this.newPassword;
+      disable = false;
+    } else {
+      disable = true;
+    }
+    return disable;
+  }
+  cancel() {
+    window.location = 'homeTutor';
   }
 }
